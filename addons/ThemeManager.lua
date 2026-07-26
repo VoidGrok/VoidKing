2e579e582d47f83355d1158054eeb96c1e19d844
VoidGrok <ramongrok11@gmail.com>
2026-07-26 05:30:09 -0300

    Create ThemeManager.lua

---
+++ addons/ThemeManager.lua
@@ -0,0 +1,10 @@
+--[[
+    VoidKing ThemeManager
+    Temas e cores da interface
+]]
+
+local BASE = "https://raw.githubusercontent.com/VoidGrok/VoidKing/main/"
+
+local ThemeManager = loadstring(game:HttpGet(BASE .. "addons/ThemeManager.lua"))()
+
+return ThemeManager
